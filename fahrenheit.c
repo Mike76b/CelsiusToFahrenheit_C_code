a22ac3a0a51017de148b10bd7345ce61232407d6
Mike <36168675+Mike76b@users.noreply.github.com>
2018-12-25 12:30:37 +0100

    Update fahrenheit.c

--- fahrenheit.c
+++ fahrenheit.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-    float C = get_float("C: "); // initializing Celsius variable
+    float C = get_float("Enter Celsius degrees: "); // initializing Celsius variable
     float F = ((C * 9) / 5) + 32; // initializing the Fahrenheit conversion
-    printf("F: %.1f\n", F); // printing the result
+    printf("Temperature in Fahrenheit degrees is: %.1f\n", F); // printing the result
 }
